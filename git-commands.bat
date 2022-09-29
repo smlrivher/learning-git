da81b7e6f5481b6dac362ad80413af3a1540e2d3
smlrivher <smlplusplus@gmail.com>
2022-09-29 14:19:25 -0500

    Estatus corto

--- git-commands.bat
+++ git-commands.bat
@@ -6,4 +6,5 @@ git init //Para inicializar git en un directorio
 git rm git-commands.html //Para eliminar archivos con comando git
 git mv git-commands.txt git-commands.bat //Para renombrar archivos con comando git
 git restore --staged git-commands.html //Para sacar el cambio despues del commit
-git restore git-commands.html //Para descartar los cambios 
+git restore git-commands.html //Para descartar los cambios
+git status -s //Para ver estatus mas pro
